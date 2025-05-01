ea8f737a0bf446db478d1d07fb36ee7fdcf08984
jeet-thakurela <119715633+jeet-thakurela@users.noreply.github.com>
2025-05-01 12:08:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
-export default {
-  build: {
-    rollupOptions: {
-      external: ['firebase/auth']
-    }
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+
